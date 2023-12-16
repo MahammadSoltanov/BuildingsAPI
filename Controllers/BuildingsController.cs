60f11e50564336b4d9fbd43181ac4d5a14855a6f
MaqaSoltanov <113199901+MaqaSoltanov@users.noreply.github.com>
2023-12-16 10:05:38 +0400

    Added logic for PUT and POST requests

--- Controllers/BuildingsController.cs
+++ Controllers/BuildingsController.cs
@@ -1,7 +1,12 @@
 ﻿using BuildingsAPI.Helpers;
 using BuildingsAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using NetTopologySuite.IO.Converters;
+using NetTopologySuite.IO;
+using Newtonsoft.Json;
 using Microsoft.EntityFrameworkCore;
+using BuildingsAPI.CustomModels;
+using NetTopologySuite.Geometries;
 
 namespace BuildingsAPI.Controllers
 {
@@ -22,7 +27,85 @@ namespace BuildingsAPI.Controllers
         {
             var buildings = await _context.Binas.ToListAsync();
 
-            return Ok(buildings.ToGeoJson()); 
+            return Ok(buildings.ToGeoJson());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBuildingById(int id)
+        {
+            var building = await _context.Binas.FindAsync(new object[] { id });
+
+            return Ok(building.ToGeoJson());
+        }
+
+        [HttpGet("getPoi/{id}")]
+        public async Task<IActionResult> GetPointsWithinBuildingById(int id)
+        {
+            var building = await _context.Binas.FindAsync(new object[] { id });
+
+
+            return Ok(building.ToGeoJson());
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> ChangeBuildingById(int id, [FromBody] GeoJsonFeature geoJsonFeature)
+        {
+            var buildingToChange = await _context.Binas.FindAsync(new object[] { id });
+            UpdateBina(buildingToChange, geoJsonFeature);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBuilding([FromBody] GeoJsonFeature geoJsonFeature)
+        {
+            var building = new Bina()
+            {
+                Hendese = GetPolygonFromGeoJson(geoJsonFeature),
+                Geotype = geoJsonFeature.Properties.Geotype,
+                Index = geoJsonFeature.Properties.Index
+            };
+
+
+            _context.Binas.Add(building);
+            await _context.SaveChangesAsync();            
+
+            return CreatedAtAction(nameof(GetBuildingById), new { id = building.Id }, building.ToGeoJson());
+        }
+
+
+        private Polygon GetPolygonFromGeoJson(GeoJsonFeature geoJsonFeature)
+        {
+            var innerList = geoJsonFeature.Geometry.Coordinates[0].ToList();
+            var coordinates = innerList.Select(points => new Coordinate(points[0], points[1])).ToArray();
+            var linearRing = new LinearRing(coordinates);
+            var polygon = new Polygon(linearRing);
+
+            return polygon;
+        }
+
+        private void UpdateBina(Bina buildingToChange, GeoJsonFeature geoJsonFeature) 
+        {
+            var newProperties = geoJsonFeature.Properties;
+
+            buildingToChange.Hendese = GetPolygonFromGeoJson(geoJsonFeature);
+            buildingToChange.AddrCity = newProperties.AddrCity;
+            buildingToChange.Name = newProperties.Name;
+            buildingToChange.AddrCountry = newProperties.AddrCountry;
+            buildingToChange.AddrCity = newProperties.AddrCity;
+            buildingToChange.AddrHousenumber = newProperties.AddrHousenumber;
+            buildingToChange.AddrPostcode = newProperties.AddrPostcode;
+            buildingToChange.AddrStreet = newProperties.AddrStreet;
+            buildingToChange.NameAz = newProperties.NameAz;
+            buildingToChange.NameRu = newProperties.NameRu;
+            buildingToChange.NameEn = newProperties.NameEn;
+            buildingToChange.Building = newProperties.Building;
+            buildingToChange.BuildingLevels = newProperties.BuildingLevels;
+            buildingToChange.Index = newProperties.Index;
         }
     }
 }
---
+++ CustomModels/CustomGeoJson.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace BuildingsAPI.CustomModels;
+
+public class GeoJsonFeature
+{
+    public string Type { get; set; }
+    public GeoJsonGeometry Geometry { get; set; }
+    public GeoJsonProperties Properties { get; set; }
+
+}
+public class GeoJsonGeometry
+{
+    public string Type { get; set; }
+    public List<List<List<double>>> Coordinates { get; set; }
+}
+
+public class GeoJsonProperties
+{
+    public int Index { get; set; }
+    public string Geotype { get; set; }
+    [JsonPropertyName("addr:city")]
+    public string AddrCity { get; set; }
+    [JsonPropertyName("addr:country")]
+    public string AddrCountry { get; set; }
+    [JsonPropertyName("addr:housenumber")]
+    public string AddrHousenumber { get; set; }
+    [JsonPropertyName("addr:postcode")]
+    public string AddrPostcode { get; set; }
+    [JsonPropertyName("addr:street")]
+    public string AddrStreet { get; set; }
+    public string Building { get; set; }
+    [JsonPropertyName("building:levels")]
+    public string BuildingLevels { get; set; }
+    public string Name { get; set; }
+    [JsonPropertyName("name:az")]
+    public string NameAz { get; set; }
+    [JsonPropertyName("name:en")]
+    public string NameEn { get; set; }
+    [JsonPropertyName("name:ru")]
+    public string NameRu { get; set; }
+
+}
